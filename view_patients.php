2b568da2c718e23234f898d68f162b1c4c97fce2
Nick-Phaita <nickphaita@gmail.com>
2023-05-31 09:47:11 +0300

    Updated view_patients.php

--- view_patients.php
+++ view_patients.php
@@ -4,4 +4,11 @@ require_once("connection.php");
 $sqlretrieve = "SELECT * from Patients";
 $result = $conn->query($sqlretrieve);
 print_r($result);
+$row = $result->fetch_assoc();
+echo "<br>";
+print_r($row);
+echo "<br>";
+echo $row['Names'];
+echo "<br>";
+echo $row['Gender'];
 ?>
