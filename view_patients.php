fab5c694fc6179647eba96bf258fcf06d6411cea
Nick-Phaita <nickphaita@gmail.com>
2023-05-31 10:10:01 +0300

    Updated view_patients.php to display records

--- view_patients.php
+++ view_patients.php
@@ -4,11 +4,26 @@ require_once("connection.php");
 $sqlretrieve = "SELECT * from Patients";
 $result = $conn->query($sqlretrieve);
 print_r($result);
-$row = $result->fetch_assoc();
+//$row = $result->fetch_assoc();
 echo "<br>";
-print_r($row);
+/*print_r($row);
 echo "<br>";
 echo $row['Names'];
 echo "<br>";
 echo $row['Gender'];
+echo "<br>";*/
+//Place in table
+if($result->num_rows > 0){
+    /*foreach($row as $key=>$value){
+        echo $key . " is " . $value . "<br>";
+    }*/
+    while($row = $result->fetch_assoc()){
+        echo "SSN: ". $row['SSN'] . " Name: ". $row['Names'] . " Gender: ". $row['Gender'] . "<br>"; //to add remaining values
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
 ?>
