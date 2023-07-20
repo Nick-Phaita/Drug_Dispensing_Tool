2d7a3909f0560a10b2a480101b1362078970e919
Nick-Phaita <nickphaita@gmail.com>
2023-07-20 05:40:11 +0300

    Styling Patient's experience

--- App/dashboards/patient_dashboard.php
+++ App/dashboards/patient_dashboard.php
@@ -8,15 +8,77 @@ if (isset( $_SESSION['loggedin'])) {
 <html>
 <head>
 	<title>Patient Dashboard</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../styles/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css">
 </head>
 <body>
-     <h1 style="text-align: right;">Hello, <?php echo $_SESSION['Names']; ?></h1>
-     <p style="text-align: left;"><a href="../signout.php">Sign Out</a></p>
-     <p><a href="../update/edit_user.php">Edit Profile</a></p>
-     <p><a href="../update/edit_patient.php">Edit Patient Details</a></p>
-     <p><a href="../view/view_prescriptions.php">View Prescriptions</a></p>
-     <p><a href="../view/view_dispensations.php">View Current Medication</a></p>
+    <div class="dashboard">
+        <div class="header">
+            <div class="icon">
+                <h2 class="logo">Dispenzer</h2>
+            </div>
+
+            <div class="greetings">
+                <h1><?php echo $_SESSION['Username']; ?></h1>
+                <h3>Hello, <?php echo $_SESSION['Names']?></h3>
+            </div>
+
+            <h2>Patient Dashboard</h2>
+        </div>
+
+        <hr>
+
+        <div class="main">
+
+            <div class="links-bar">
+                <ul>
+                    <li><a href="../update/edit_user.php">Edit Profile</a></li>
+                    <li><a href="../update/edit_patient.php">Edit Patient Details</a></li>
+                    <li><a href="../view/view_prescriptions.php">View Prescriptions</a></li>
+                    <li><a href="../view/view_dispensations.php">View Medication</a></li>
+                    <li><a href="../signout.php">Sign Out</a></li>
+                </ul>
+    
+            </div>
+
+        
+            <div class="capabilities">
+                <h2>Access all functionalities from the sidebar. Hover over each below to see what you can do.</h2>
+                <div class="capability">
+                    <h4>Edit Profile</h4>
+                    <p>Update your user details such as your Username and Password.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Edit Patient Details</h4>
+                    <p>Update your patient details such as SSN, Names, Allergies and Height, among others.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Prescriptions</h4>
+                    <p>View all prescriptions made for you so far.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Medication</h4>
+                    <p>View all collected prescriptions.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Sign Out</h4>
+                    <p>Logout from your account.</p>
+                </div>
+                
+            </div>
+
+            
+        
+        </div>
+
+    </div>
+    
+     
+     
 </body>
 </html>
 
--- App/registration/registerPatient.php
+++ App/registration/registerPatient.php
@@ -7,38 +7,88 @@ require_once("../signup.php");
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register Patient</title>
-        <link rel="stylesheet" type="text/css" href="">
+        <link rel="stylesheet" type="text/css" href="../styles/style.css">
+        <!--<link rel="stylesheet" type="text/css" href="../styles/form.css">-->
+        <script type="text/javascript" src="../scripts.js"></script>
+        <link rel="stylesheet" type="text/css" href="../styles/form.css">
+        <link rel="stylesheet" type="text/css" href="../styles/style.css">
 
     </head>
     <body>
-        <h1>Patient sign-up</h1>
-        <form method="post" action="register_patient.php">
-            <label for="Username">Username: </label><br>
-            <input type="text" name="Username" id="Username" required value="<?php echo $_SESSION['Username']?>" readonly><br>
-            <label for="SSN">Social Security Number:</label><br>
-            <input type="text" id="SSN" name="SSN" required placeholder="Enter your SSN"><br>
-            <label for="Names">Name: </label><br>
-            <input type="text" id="Names" name="Names" required placeholder="Enter your name here"><br>
-            <label for="Gender">Gender:</label><br>
-            <select name="Gender" id="Gender" required>
-                <option value="" selected hidden >Choose Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="Non-binary">Non-binary</option>
-            </select><br>
-            <label for="Allergies">Allergies:</label><br>
-            <textarea name="Allergies" id="Allergies" cols="20" rows="10"></textarea><br>
-            <label for="HeightinCm">Height (in centimetres):</label><br>
-            <input type="text" id="HeightinCm" name="HeightinCm" placeholder="Enter your height" required><br>
-            <label for="WeightinKg">Weight (in kilograms):</label><br>
-            <input type="text" id="WeightinKg" name="WeightinKg" placeholder="Enter your weight" required><br>
-            <label for="PatientAddress">Address:</label><br>
-            <input type="text" name="PatientAddress" id="PatientAddress" required placeholder="Enter your address"><br>
-            <label for="DateOfBirth">Date of birth: </label><br>
-            <input type="date" name="DateOfBirth" id="DateOfBirth" required><br>
-            <label for="PrimaryPhysicianSSN">Primary Physician SSN:</label><br>
-            <input type="text" id="PrimaryPhysicianSSN" name="PrimaryPhysicianSSN" required placeholder="Enter your physician's SSN"><br>
-            <input type="submit" value="Submit">
-        </form>
+        <div class="form">
+            <div class="left-container">
+                <div class="icon">
+                    <h2 class="logo">Dispenzer</h2>
+                </div>
+                <h1>Patient sign-up</h1>
+            </div>
+            <div class="right-container">
+                <form method="post" action="register_patient.php">
+                    <div class="field">
+                        <label for="Username">Username: </label><br>
+                        <input type="text" name="Username" id="Username" required value="<?php echo $_SESSION['Username']?>" readonly><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="SSN">Social Security Number:</label><br>
+                        <input type="text" id="SSN" name="SSN" required placeholder="Enter your SSN"><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="Names">Name: </label><br>
+                        <input type="text" id="Names" name="Names" required placeholder="Enter your name here"><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="Gender">Gender:</label><br>
+                        <select name="Gender" id="Gender" required>
+                            <option value="" selected hidden >Choose Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Non-binary">Non-binary</option>
+                        </select><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="HeightinCm">Height (in centimetres):</label><br>
+                        <input type="text" id="HeightinCm" name="HeightinCm" placeholder="Enter your height" required><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="WeightinKg">Weight (in kilograms):</label><br>
+                        <input type="text" id="WeightinKg" name="WeightinKg" placeholder="Enter your weight" required><br>
+                    </div>
+
+                    <div class="field-txtarea">
+                        <label for="Allergies">Allergies:</label><br>
+                        <textarea name="Allergies" id="Allergies" cols="20" rows="10"></textarea><br>
+                    </div>
+
+
+                    <div class="field">
+                        <label for="PatientAddress">Address:</label><br>
+                        <input type="text" name="PatientAddress" id="PatientAddress" required placeholder="Enter your address"><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="DateOfBirth">Date of birth: </label><br>
+                        <input type="date" name="DateOfBirth" id="DateOfBirth" required><br>
+                    </div>
+
+                    <div class="field">
+                        <label for="PrimaryPhysicianSSN">Primary Physician SSN:</label><br>
+                        <input type="text" id="PrimaryPhysicianSSN" name="PrimaryPhysicianSSN" required placeholder="Enter your physician's SSN"><br>
+                    </div>
+
+                    <input class="button" type="submit" value="Submit">
+                    <input class="button" type="reset" onclick="return confirm_reset();">
+                    <button class="button" onclick="cancel()">Cancel</button>
+                </form>
+
+            </div>
+            
+            
+        </div>
+
     </body> 
 </html>
