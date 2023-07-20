42472bcb959e667adf8b61e751337cd5db96802a
Chegesta <chegegitiche254@gmail.com>
2023-07-20 14:23:18 +0300

    modification of the dashboards

--- App/dashboards/admin_dashboard.php
+++ App/dashboards/admin_dashboard.php
@@ -8,14 +8,67 @@ if (isset( $_SESSION['loggedin'])) {
 <html>
 <head>
 	<title>Administrator Dashboard</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../styles/dashboard.css">
+	<link rel="stylesheet" type="text/css" href="../styles/style.css">
 </head>
 <body>
-     <h1 style="text-align: right;">Hello, Administrator</h1>
-     <p style="text-align: left;"><a href="../signout.php">Sign Out</a></p>
-     <p><a href="../update/edit_user.php">Edit Profile</a></p>
-     <p><a href="../add/add_pharmacy.php">Add pharmacy</a></p>
-     <p><a href="../view/view_users.php">View Users</a></p>
+<div class="dashboard">
+        <div class="header">
+            <div class="icon">
+                <h2 class="logo">Dispenzer</h2>
+            </div>
+
+            <div class="greetings">
+               <h1>Hello, Administrator</h1>
+            </div>
+
+            <h2>Patient Dashboard</h2>
+        </div>
+
+        <hr>
+
+        <div class="main">
+
+            <div class="links-bar">
+                <ul>
+                    <li><a href="../update/edit_user.php">Edit Profile</a></li>
+                    <li><a href="../add/add_pharmacy.php">Add pharmacy</a></li>
+                    <li><a href="../view/view_users.php">View Users</a></li>
+                    <li><a href="../signout.php">Sign Out</a></li>
+                </ul>
+    
+            </div>
+
+        
+            <div class="capabilities">
+                <h2>Access all functionalities from the sidebar. Hover over each below to see what you can do.</h2>
+                <div class="capability">
+                    <h4>Edit Profile</h4>
+                    <p>Update your user details such as your Username and Password.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Add pharmacy</h4>
+                    <p>Add Pharmacies to the system</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Users</h4>
+                    <p>View all users able to access the system</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Sign Out</h4>
+                    <p>Logout from your account.</p>
+                </div>
+                
+            </div>
+
+            
+        
+        </div>
+
+    </div>
 </body>
 </html>
 
--- App/dashboards/pharmacist_dashboard.php
+++ App/dashboards/pharmacist_dashboard.php
@@ -8,19 +8,98 @@ if (isset( $_SESSION['loggedin'])) {
 <html>
 <head>
 	<title>Pharmacist Dashboard</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="../styles/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css">
 </head>
 <body>
-     <h1 style="text-align: right;">Hello, <?php echo $_SESSION['Names']; ?></h1>
-     <p style="text-align: left;"><a href="../signout.php">Sign Out</a></p>
-     <p><a href="../update/edit_user.php">Edit Profile</a></p>
-     <p><a href="../update/edit_pharmacist.php">Edit Details</a></p>
-     <p><a href="../add/add_dispensation.php">Make dispensation</a></p>
-     <p><a href="../view/view_prescriptions.php">View Prescriptions</a></p>
-     <p><a href="../view/view_patients.php">View Patients</a></p>
-     <p><a href="../view/view_inventory.php">View Inventory</a></p>
-     <p><a href="../view/view_doctors.php">View Doctors</a></p>
-     <p><a href="../view/view_dispensations.php">View Dispensations</a></p>
+<div class="dashboard">
+        <div class="header">
+            <div class="icon">
+                <h2 class="logo">Dispenzer</h2>
+            </div>
+
+            <div class="greetings">
+                <h1><?php echo $_SESSION['Username']; ?></h1>
+                <h3>Hello, <?php echo $_SESSION['Names']?></h3>
+            </div>
+
+            <h2>Pharmacist Dashboard</h2>
+        </div>
+
+        <hr>
+
+        <div class="main">
+
+            <div class="links-bar">
+                <ul>
+                    <li><a href="../update/edit_user.php">Edit Profile</a></li>
+                    <li><a href="../update/edit_pharmacist.php">Edit Details</a></li>
+                    <li><a href="../view/view_prescriptions.php">View Prescriptions</a></li>
+                    <li><a href="../add/add_dispensation.php">Make dispensation</a></li>
+                    <li><a href="../view/view_dispensations.php">View Dispensation</a></li>
+                    <li><a href="../view/view_patients.php">View Patients</a></li>
+                    <li><a href="../view/view_inventory.php">View Inventory</a></li>
+                    <li><a href="../view/view_doctors.php">View Doctors</a></li>
+                    <li><a href="../signout.php">Sign Out</a></li>
+                </ul>
+    
+            </div>
+
+        
+            <div class="capabilities">
+                <h2>Access all functionalities from the sidebar. Hover over each below to see what you can do.</h2>
+                <div class="capability">
+                    <h4>Edit Profile</h4>
+                    <p>Update your user details such as your Username and Password.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Edit Details</h4>
+                    <p>Update your pharmacist details.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Prescriptions</h4>
+                    <p>View all prescriptions made for you so far.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Make dispensation</h4>
+                    <p>Make a dispensation to a patient.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View dispensation</h4>
+                    <p>View all drugs dispensed to patients.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View patients</h4>
+                    <p>View all recorded patients in the system.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Inventory</h4>
+                    <p>View the entire stock found in the system.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>View Doctors</h4>
+                    <p>View all doctors in the system.</p>
+                </div>
+
+                <div class="capability">
+                    <h4>Sign Out</h4>
+                    <p>Logout from your account.</p>
+                </div>
+                
+            </div>
+
+            
+        
+        </div>
+
+    </div>
 </body>
 </html>
 
